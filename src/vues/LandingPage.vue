4a562d06de369b5ccfa092746acc075c1c0d5963
Romain Bazin <romain.bzin@gmail.com>
2023-07-16 09:13:20 +0200

    recording function

--- src/vues/LandingPage.vue
+++ src/vues/LandingPage.vue
@@ -26,15 +26,18 @@
         <!-- Voice recording button -->
         <section class="section is-flex is-flex-direction-horizontal is-justify-content-center is-align-items-center">
             <div class="container has-text-centered">
-                <span @click="toggleRecording" class="icon-text" :class="recordingColor" style="cursor: pointer;">
-                    <span class="icon is-size-1" :class="recordingColor">
-                        <!-- <i class="fa-solid fa-microphone"></i> -->
-                        <font-awesome-icon :icon="'fa-solid fa-microphone'"/>
+                <div class="button is-rounded is-primary">
+                    <span @click="toggleRecording" class="icon-text is-align-items-center" :class="recordingColor"
+                        style="cursor: pointer;">
+                        <span class="icon is-size-3" :class="recordingColor">
+                            <!-- <i class="fa-solid fa-microphone"></i> -->
+                            <font-awesome-icon :icon="'fa-solid fa-microphone'" />
+                        </span>
+                        <span class="is-size-5">
+                            {{ recordingText }}
+                        </span>
                     </span>
-                    <span>
-                        {{ recordingText }}
-                    </span>
-                </span>
+                </div>
             </div>
         </section>
     </div>
@@ -43,6 +46,7 @@
 <script>
 import ItemComponent from '@/components/ItemComponent.vue'
 import { noteStore } from '@/stores/noteStore'
+import { RecordRTCPromisesHandler, invokeSaveAsDialog, StereoAudioRecorder } from 'recordrtc';
 
 export default {
     name: 'LandingPage',
@@ -58,26 +62,41 @@ export default {
     },
     data() {
         return {
-            recording: false
+            recording: false,
+            recorder: null,
         }
     },
     methods: {
-        toggleRecording() {
+        async toggleRecording() {
             if (this.recording) {
                 console.log("Stop recording")
+                await this.recorder.stopRecording();
+                let blob = await this.recorder.getBlob();
+                invokeSaveAsDialog(blob, 'audio.wav');
+                this.recorder.destroy();
+                this.recorder = null;
             }
             else {
                 console.log("Start recording")
+                let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+                this.recorder = new RecordRTCPromisesHandler(stream, {
+                    type: 'audio',
+                    mimeType: 'audio/wav',
+                    recorderType: StereoAudioRecorder,
+                    desiredSampRate: 16000,
+                    numberOfAudioChannels: 1,
+                });
+                this.recorder.startRecording();
             }
             this.recording = !this.recording
         },
     },
     computed: {
         recordingText() {
-            return this.recording ?  "Click to stop recording": "Click to start recording"
+            return this.recording ? "Click to stop recording" : "Click to start recording"
         },
         recordingColor() {
-            return this.recording ?  "has-text-danger" : "has-text-white" 
+            return this.recording ? "has-text-danger" : "has-text-white"
         }
     }
 }
